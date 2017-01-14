3eaead7a96b39b6a29b72d74f3298206a562c11b
Lahiru Ananda <lahiru.annada@infor.com>
2017-01-14 21:50:32 +0530

    ENH-0001: Rest with Node.js and Express. Code setup

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+//create router to be used bt Books API
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get(function(request, response){
+    Book.find(function(err, books){
+      if(err){
+        response.status(500).send(err);
+      } else {
+        response.json(books);
+      }
+    });
+    // var responseJson = {
+    //   hello: "This is my API"
+    // }
+    // response.json(responseJson);
+  });
+
+app.use('/api', bookRouter);
+
 
 app.get('/', function(request, response){
   response.send('Welcome to my API');
 });
 
 app.listen(port, function(){
-  console.log("Running on PORT: " + port)
+  console.log("GULP is running on PORT: " + port)
 });
