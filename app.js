4ffa1a90e87baf763620024a0aa6f996f0986d78
Lahiru Ananda <lahiru.annada@infor.com>
2017-01-14 22:08:10 +0530

    ENH-0001: Rest with Node.js and Express. Query V2

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ var bookRouter = express.Router();
 bookRouter.route('/Books')
   .get(function(request, response){
     //create query
-    var query = request.query;
+    var query = {};
+    if(request.query.genre){
+      query.genre = request.query.genre;
+    }
+
     Book.find(query, function(err, books){
       if(err){
         response.status(500).send(err);
       } else {
         response.json(books);
+      }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+  .get(function(request, response){
+    Book.findById(request.params.bookId, function(err, book){
+      if(err){
+        response.status(500).send(err);
+      } else{
+        response.json(book);
       }
     });
   });
